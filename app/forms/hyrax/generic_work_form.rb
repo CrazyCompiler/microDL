46ee4b5f7cbdbeaa32d738b82da8b88dfec6aa3f
Viraj Parab <parabveer01@gmail.com>
2018-07-31 15:14:22 +0530

    Update related url tag to related works

--- app/forms/hyrax/generic_work_form.rb
+++ app/forms/hyrax/generic_work_form.rb
@@ -4,6 +4,11 @@ module Hyrax
   # Generated form for GenericWork
   class GenericWorkForm < Hyrax::Forms::WorkForm
     self.model_class = ::GenericWork
-    self.terms += [:resource_type]
+    self.terms = [:title, :creator, :contributor, :description,
+                  :keyword, :license, :date_created,
+                  :subject,  :identifier, :related_url,
+                  :representative_id, :thumbnail_id, :rendering_ids, :files]
+
+    self.required_fields = [:title, :creator, :keyword, :subject]
   end
 end
