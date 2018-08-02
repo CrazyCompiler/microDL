b2db5fcd447b272113cd3438fec9e13efec6c842
Viraj Parab <parabveer01@gmail.com>
2018-08-02 15:19:44 +0530

    Add actor to make related work property bidirectional for edit action

--- app/actors/hyrax/actors/generic_work_actor.rb
+++ app/actors/hyrax/actors/generic_work_actor.rb
@@ -9,6 +9,12 @@ module Hyrax
         save(env) && next_actor.create(env) && run_callbacks(:after_create_concern, env) && update_related_works(env)
       end
 
+      def update(env)
+        apply_update_data_to_curation_concern(env)
+        apply_save_data_to_curation_concern(env)
+        next_actor.update(env) && save(env) && run_callbacks(:after_update_metadata, env) && update_related_works(env)
+      end
+
       def update_related_works(env)
         current_work_ID = env.curation_concern.id
         related_work_ids = env.attributes["related_url"]
@@ -16,9 +22,11 @@ module Hyrax
           work = ::ActiveFedora::Base.find(work_ID)
           related_urls = work.attributes().values_at("related_url")
           related_urls = related_urls.to_a[0].to_a
-          related_urls << current_work_ID
-          work.update({"related_url" => related_urls})
-          work.save!
+          unless related_urls.include? current_work_ID
+            related_urls << current_work_ID
+            work.update({"related_url" => related_urls})
+            work.save!
+          end
         end
       end
     end
